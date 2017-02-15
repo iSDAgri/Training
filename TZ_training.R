32e943cad3869763c3278a5c4f10c5efd5f99f6d
mgwalsh <markusgwalsh@gmail.com>
2017-02-15 10:48:33 +0300

    TZ training script for loading mobile survey and Tanzania grids

--- TZ_training.R
+++ TZ_training.R
@@ -9,6 +9,7 @@ require(rgdal)
 dir.create("data", showWarnings=F)
 setwd("./data")
 
+# load TZ grids
 download("https://www.dropbox.com/s/nnoehn7wbwtqvk7/TZ_grids1k.zip?dl=0", "TZ_grids1k.zip",  mode="wb")
 unzip("TZ_grids1k.zip", overwrite=T)
 glist <- list.files(pattern="tif", full.names=T)
@@ -33,14 +34,5 @@ download.file("https://www.dropbox.com/s/02g8dmzvr18nyx3/Crop_TZ_JAN_2017.csv.zi
 unzip("Crop_TZ_JAN_2017.csv.zip", overwrite=T)
 mob <- read.table("Crop_TZ_JAN_2017.csv.zip", header=T, sep=",")
 
-geo.proj <- as.data.frame(project(cbind(geo$Lon, geo$Lat), "+proj=laea +ellps=WGS84 +lon_0=20 +lat_0=5 +units=m +no_defs"))
-colnames(geo.proj) <- c("x","y") ## laea coordinates
-geo <- cbind(geo, geo.proj)
-coordinates(geo) <- ~x+y
-projection(geo) <- projection(grids)
-geogrd <- extract(grids, geo)
-geogrd <- as.data.frame(geogrd)
-geo <- cbind.data.frame(geo, geogrd)
-geo <- unique(na.omit(geo)) ## includes only unique & complete records
 
 
