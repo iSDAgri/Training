ad7666e10eac6fd852a8a7332a51d2d43a8b4815
mgwalsh <markusgwalsh@gmail.com>
2017-02-15 10:46:50 +0300

    TZ training script for loading mobile survey and Tanzania grids

--- TZ_training.R
+++ TZ_training.R
@@ -14,13 +14,24 @@ unzip("TZ_grids1k.zip", overwrite=T)
 glist <- list.files(pattern="tif", full.names=T)
 grids <- stack(glist)
 
+# load GeoSurvey data
 download.file("https://www.dropbox.com/s/skdinhb5iing6mg/TZ_geos_012015.csv?dl=0", "./TZ_geos_012015.csv", mode= "wb")
 geo <- read.table("TZ_geos_012015.csv", header=T, sep=",")
+plot(geo$Lon,geos$Lat, cex=0.01)
+geo.proj <- as.data.frame(project(cbind(geo$Lon, geo$Lat), "+proj=laea +ellps=WGS84 +lon_0=20 +lat_0=5 +units=m +no_defs"))
+colnames(geo.proj) <- c("x","y") ## laea coordinates
+geo <- cbind(geo, geo.proj)
+coordinates(geo) <- ~x+y
+projection(geo) <- projection(grids)
+geogrd <- extract(grids, geo)
+geogrd <- as.data.frame(geogrd)
+geo <- cbind.data.frame(geo, geogrd)
+geo <- unique(na.omit(geo)) ## includes only unique & complete records
 
+# load MobileSurvey data
 download.file("https://www.dropbox.com/s/02g8dmzvr18nyx3/Crop_TZ_JAN_2017.csv.zip?dl=0","./data/Crop_TZ_JAN_2017.csv.zip", mode="wb")
 unzip("Crop_TZ_JAN_2017.csv.zip", overwrite=T)
 mob <- read.table("Crop_TZ_JAN_2017.csv.zip", header=T, sep=",")
-plot(geos$Lon,geos$Lat, cex=0.01)
 
 geo.proj <- as.data.frame(project(cbind(geo$Lon, geo$Lat), "+proj=laea +ellps=WGS84 +lon_0=20 +lat_0=5 +units=m +no_defs"))
 colnames(geo.proj) <- c("x","y") ## laea coordinates
