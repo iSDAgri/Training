8850efe50bf32ed42daf40267b7bc72f1e300d23
mgwalsh <markusgwalsh@gmail.com>
2017-02-17 07:33:52 +0300

    superceded

--- TZ_training_glm.R
+++ TZ_training_glm.R
@@ -1,13 +1,14 @@
-#' Example glmnet model of Tanzania mobile survey data with gridded covariates
+#' Example spatial models of Tanzania mobile survey data with gridded covariates
 #' M. Walsh & A. Verlinden, February 2017
 
 # Required packages
-# install.packages(c("devtools","caret","raster","glmnet","doParallel")), dependencies=TRUE)
+# install.packages(c("devtools","caret","raster","glmnet","randomForest","doParallel")), dependencies=TRUE)
 suppressPackageStartupMessages({
   require(devtools)
   require(caret)
   require(raster)
   require(glmnet)
+  require(randomForest)
   require(doParallel)
 })
 
@@ -46,6 +47,30 @@ mob.glm <- train(grdt, PAt,
 print(mob.glm)
 mob.imp <- varImp(mob.glm)
 plot(mob.imp, top=25, col="black", cex=1.3, xlab="Variable importance", cex.lab=1.5)
-mob_pred <- predict(grids, mob.glm, type="prob") ## spatial predictions
-plot(1-mob_pred, axes=F) ## initial map
+mob_glm <- predict(grids, mob.glm, type="prob") ## spatial predictions
+plot(1-mob_glm, axes=F) ## probaility map plot
 
+stopCluster(mc)
+
+# Fit randomForest model --------------------------------------------------
+# Start doParallel to parallelize model fitting
+mc <- makeCluster(detectCores())
+registerDoParallel(mc)
+
+# Control setup
+set.seed(1385321)
+tc <- trainControl(method = "cv", classProbs = TRUE, summaryFunction = twoClassSummary,
+                   allowParallel = T)
+
+# Fit randomForest model
+mob.rf <- train(grdt, PAt,
+                preProc = c("center", "scale"),
+                method = "rf",
+                family = "binomial",
+                metric = "ROC",
+                trControl = tc)
+print(mob.rf)
+mob.imp <- varImp(mob.rf)
+plot(mob.imp, top=25, col="black", cex=1.3, xlab="Variable importance", cex.lab=1.5)
+mob_rf <- predict(grids, mob.rf, type="prob") ## spatial predictions
+plot(1-mob_rf, axes=F) ## probaility map plot
